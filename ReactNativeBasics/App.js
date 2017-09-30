c3cbaa3db49610ae58d7406adf40e9fd7790e812
Peiyu Liao <u3512485@connect.hku.hk>
2017-09-30 17:17:19 +0800

    Finish basic

--- ReactNativeBasics/App.js
+++ ReactNativeBasics/App.js
@@ -1,13 +1,44 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableHighlight } from 'react-native';
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+
+    // Define states
+    this.state = {
+      titleColor: 'red',
+    };
+
+    // Bind callback context
+    this.changeColor = this.changeColor.bind(this);    
+  }
+
+  changeColor() {
+    const newColor = `rgb(
+      ${parseInt(Math.random() * 255)}, 
+      ${parseInt(Math.random() * 255)}, 
+      ${parseInt(Math.random() * 255)}
+    )`;
+
+    // Set new state, trigger render function
+    this.setState({
+      titleColor: newColor,
+    });
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <TouchableHighlight 
+          style={styles.logoContainer}
+          onPress={this.changeColor}
+        >
+          <Image source={require('./sitcon_logo.png')} />
+        </TouchableHighlight>
+        <View style={styles.titleContainer}>
+          <Text style={[styles.title, { color: this.state.titleColor }]}>SITCON HK 2017</Text>
+        </View>
       </View>
     );
   }
@@ -19,5 +50,18 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    flexDirection: 'row',
+  },
+  logoContainer: {
+    flex: 2,
+    alignItems: 'center',
+  },
+  titleContainer: {
+    flex: 1,
+    padding: 10,
+  },
+  title: {
+    fontSize: 24,
+    textAlign: 'center',
   },
 });
